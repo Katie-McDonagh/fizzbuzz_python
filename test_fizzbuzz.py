38755302046665c6e0feba4384e98a4350d58d61
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-04 17:14:39 +0000

    red test -returns one when given one

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,7 +3,11 @@ import unittest
 from fizz_buzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-
+  def test_list(self):
+    'returns 1 when given one'
+    data = 1
+    result = fizz_buzz(data)
+    self.assertEqual(result, 1)
 
 if __name__ == '__main__':
   unittest.main() 
